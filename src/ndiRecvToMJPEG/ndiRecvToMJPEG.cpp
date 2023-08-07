6c66847e8d28ecbce01b496b25b70c1337ef6ada
Keith Senti <ksenti@gmail.com>
2023-08-07 16:04:44 -0500

    try streamer 1

--- src/ndiRecvToMJPEG/ndiRecvToMJPEG.cpp
+++ src/ndiRecvToMJPEG/ndiRecvToMJPEG.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <cstring>
 #include <Processing.NDI.Lib.h>
+#include <nadjieb/mjpeg_streamer.hpp>
+#include <opencv2/opencv.hpp>
 
 #ifdef _WIN32
 #ifdef _WIN64
@@ -11,14 +13,42 @@
 #endif // _WIN64
 #endif // _WIN32
 
+// for convenience
+using MJPEGStreamer = nadjieb::MJPEGStreamer;
+
+
 int main(int argc, char* argv[])
 {
-	// Exit if missing arguments
+	//
+    // Check Arguments
+    //
     if (argc < 2) {
         printf("Error: Missing command line arguments.\n");
         return -1;
     }
+    
     const char* searchSourceName = argv[1];
+
+    
+    //
+    // Setup Streamer
+    //
+    std::vector<int> params = {cv::IMWRITE_JPEG_QUALITY, 90};
+    MJPEGStreamer streamer;
+
+    // By default "/shutdown" is the target to graceful shutdown the streamer
+    // if you want to change the target to graceful shutdown:
+    //      streamer.setShutdownTarget("/stop");
+
+    // By default std::thread::hardware_concurrency() workers is used for streaming
+    // if you want to use 4 workers instead:
+    //      streamer.start(8080, 4);
+    streamer.start(8080);
+
+
+    //
+    // Setup NDI
+    //
 
     // Not required, but "correct" (see the SDK documentation).
 	if (!NDIlib_initialize())
@@ -35,7 +65,7 @@ int main(int argc, char* argv[])
 	using namespace std::chrono;
 	for (const auto start = high_resolution_clock::now(); high_resolution_clock::now() - start < minutes(1);) {
 		// Wait up till 5 seconds to check for new sources to be added or removed
-		if (!NDIlib_find_wait_for_sources(pNDI_find, 5000 /* milliseconds */)) {
+    	if (!NDIlib_find_wait_for_sources(pNDI_find, 5000 /* milliseconds */)) {
 			printf("Searching for source...\n");
 			continue;
 		}
@@ -75,29 +105,37 @@ int main(int argc, char* argv[])
 	// Destroy the NDI finder. We needed to have access to the pointers to p_sources[0]
 	NDIlib_find_destroy(pNDI_find);
 
-	// Run for one minute
-	using namespace std::chrono;
-	for (const auto start = high_resolution_clock::now(); high_resolution_clock::now() - start < minutes(1);) {
-		// The descriptors
+	//// Run for one minute
+	//using namespace std::chrono;
+    //for (const auto start = high_resolution_clock::now(); high_resolution_clock::now() - start < minutes(1);) {
+    // Visit /shutdown or another defined target to stop the loop and graceful shutdown
+    while (streamer.isRunning()) {
 		NDIlib_video_frame_v2_t video_frame;
-		NDIlib_audio_frame_v2_t audio_frame;
 
-		switch (NDIlib_recv_capture_v2(pNDI_recv, &video_frame, &audio_frame, nullptr, 5000)) {
+		switch (NDIlib_recv_capture_v3(pNDI_recv, &video_frame, nullptr, nullptr, 5000)) {
 			// No data
 			case NDIlib_frame_type_none:
 				printf("No data received.\n");
 				break;
 
-				// Video data
+			// Video data
 			case NDIlib_frame_type_video:
 				//printf("Video data received (%dx%d).\n", video_frame.xres, video_frame.yres);
-				NDIlib_recv_free_video_v2(pNDI_recv, &video_frame);
-				break;
+                
+                // http://localhost:8080/bgr
+                std::vector<uchar> buff_bgr;
+                cv::imencode(".jpg", frame, buff_bgr, params);
+                streamer.publish("/bgr", std::string(buff_bgr.begin(), buff_bgr.end()));
 
-				// Audio data
-			case NDIlib_frame_type_audio:
-				//printf("Audio data received (%d samples).\n", audio_frame.no_samples);
-				NDIlib_recv_free_audio_v2(pNDI_recv, &audio_frame);
+                cv::Mat hsv;
+                cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
+
+                // http://localhost:8080/hsv
+                std::vector<uchar> buff_hsv;
+                cv::imencode(".jpg", hsv, buff_hsv, params);
+                streamer.publish("/hsv", std::string(buff_hsv.begin(), buff_hsv.end()));
+                
+                NDIlib_recv_free_video_v2(pNDI_recv, &video_frame);
 				break;
 		}
 	}
